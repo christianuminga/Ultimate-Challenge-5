9f19bc9318828611a2281003ac0bc72b591b5b47
Christian Uminga <christian.uminga@aceproject.space>
2018-09-30 23:49:47 -0500

    created seed script to populate products table with 676 products

--- simple_store/db/seeds.rb
+++ simple_store/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+676.times do
+
+  title          = Faker::Food.dish
+  description    = Faker::Food.description
+  price          = Faker::Commerce.price
+  stock_quantity = Faker::Number.between(1, 100)
+
+  new_product = Product.create(:title          => title,
+                               :description    => description,
+                               :price          => price,
+                               :stock_quantity => stock_quantity)
+end
